226c1d4ec785b26d7723bdcb8888bb7ca49d7a51
Satan Sharma <satansharma7549@gmail.com>
2023-05-27 22:35:44 +0530

    Order redux done

---
+++ frontend/src/Redux/Order/order.Action.js
@@ -0,0 +1,50 @@
+// // get order items
+// export const GET_ORDER_ITEMS_LOADING = "order/items/get/laoding";
+// export const GET_ORDER_ITEMS_SUCCESS = "order/items/get/success";
+// export const GET_ORDER_ITEMS_ERROR = "order/items/get/error";
+
+import {
+  ADD_ORDER_ITEM_TO_ERROR,
+  ADD_ORDER_ITEM_TO_LOADING,
+  ADD_ORDER_ITEM_TO_SUCCESS,
+  GET_ORDER_ITEMS_ERROR,
+  GET_ORDER_ITEMS_LOADING,
+  GET_ORDER_ITEMS_SUCCESS,
+  REMOVE_ORDER_ITEMS_ERROR,
+  REMOVE_ORDER_ITEMS_LOADING,
+  REMOVE_ORDER_ITEMS_SUCCESS,
+} from "./order.ActionType";
+
+
+export const getAllOrdes = (data) => async (disptach) => {
+  disptach({ type: GET_ORDER_ITEMS_LOADING });
+  try {
+    //   let res = axios.get("http://localhost:8080/cartItems");
+    disptach({ type: GET_ORDER_ITEMS_SUCCESS, payload: data });
+  } catch (error) {
+    disptach({ type: GET_ORDER_ITEMS_ERROR });
+  }
+};
+
+export const addOrderItems = (orderInfo) => async (dispatch) => {
+  dispatch({ type: ADD_ORDER_ITEM_TO_LOADING });
+  try {
+    // let res = await axios.post("http://localhost:8080/cartItems", {
+    //   ...cartInfo,
+    // });
+    // let data = res.data;
+    dispatch({ type: ADD_ORDER_ITEM_TO_SUCCESS, payload: orderInfo });
+  } catch (error) {
+    dispatch({ type: ADD_ORDER_ITEM_TO_ERROR });
+  }
+};
+
+export const removeMoveOrderItem = (cartId) => async (dispatch) => {
+  dispatch({ type: REMOVE_ORDER_ITEMS_LOADING });
+  try {
+    // let res = await axios.delete(`http://localhost:8080/cartItems/${cartId}`);
+    dispatch({ type: REMOVE_ORDER_ITEMS_SUCCESS, payload: { id: cartId } });
+  } catch (error) {
+    dispatch({ type: REMOVE_ORDER_ITEMS_ERROR });
+  }
+};
