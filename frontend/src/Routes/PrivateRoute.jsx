906ab5ff4bb24ac5fd26efefe087199bcdb6bb74
Satan Sharma <satansharma7549@gmail.com>
2023-05-27 17:07:19 +0530

    Update PrivateRoute.jsx

--- frontend/src/Routes/PrivateRoute.jsx
+++ frontend/src/Routes/PrivateRoute.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
 
-
 const PrivateRoute = ({ children }) => {
   const { authData } = useSelector((store) => store.auth);
   const { pathname } = useLocation();
-  
-  if (authData.isAuthenticated) {
-    return <>{children}</>; 
+
+  if (authData.token) {
+    return <>{children}</>;
   } else {
     return <Navigate to="/login" state={{ from: pathname }} replace />;
   }
