e6d6fef755372cfa7f9b68b8c25e9ddac7d6766d
Satan Sharma <satansharma7549@gmail.com>
2023-05-28 03:02:06 +0530

    Update Home.jsx

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,14 +2,17 @@ import React, { useEffect } from "react";
 import BookLists from "../Components/BookLists";
 import { useDispatch, useSelector } from "react-redux";
 import { getBooks } from "../Redux/Books/books.Action";
+import Loading from "../Components/Loading";
 
 const Home = () => {
-  const { allBooks } = useSelector((store) => store.books);
+  const { loading, allBooks } = useSelector((store) => store.books);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getBooks());
   }, []);
+
+  if (loading) return <Loading />;
 
   return <BookLists books={allBooks} />;
 };
